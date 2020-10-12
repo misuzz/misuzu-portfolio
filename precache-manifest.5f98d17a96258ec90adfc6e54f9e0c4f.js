385be2431bc77516d526bae1e0e5dd0e69ea2db5
misuzz <fs9dm3cc8ct1@gmail.com>
2020-10-12 21:23:34 +0900

    Updates

---
+++ precache-manifest.5f98d17a96258ec90adfc6e54f9e0c4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d74c910aa10c5cda2de05874dde73c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "658450a8e75cbc87f843",
+    "url": "/static/css/main.cde5dcba.chunk.css"
+  },
+  {
+    "revision": "7e44604716a6806dda5b",
+    "url": "/static/js/2.4cb17444.chunk.js"
+  },
+  {
+    "revision": "0445d5183d0141ac77c6557b72598011",
+    "url": "/static/js/2.4cb17444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "658450a8e75cbc87f843",
+    "url": "/static/js/main.b8004ebe.chunk.js"
+  },
+  {
+    "revision": "0b99be890e689da09b4c",
+    "url": "/static/js/runtime-main.901d9af7.js"
+  },
+  {
+    "revision": "16c8a25420d601721a4edd549a6d3585",
+    "url": "/static/media/Animal-img.16c8a254.bin"
+  }
+]);
